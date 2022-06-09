eed7f8fa76440bb4516b464d58f2fe24f551df16
trantuan4132 <trantuan4132@gmail.com>
2022-06-09 22:10:39 +0700

    Minor fix

--- train.sh
+++ train.sh
@@ -1,5 +1,16 @@
-python utils/preprocess_data.py --drop
-python utils/fold_split.py
-python utils/preprocess_checkpoint.py
+python utils/preprocess_data.py \
+    --label_path "train.csv" \
+    --annot_path "train_annotations.csv" \
+    --relabel_path "relabel.csv" \
+    --drop
+python utils/fold_split.py \
+    --label_path "train.csv" \
+    --kfold 5 \
+    --annot_path "train_annotations.csv" \
+    --save_path "train_fold5.csv"
+python utils/preprocess_checkpoint.py \
+    --checkpoint_path "convnext_tiny_22k_1k_384.pth" \
+    --model "convnext" \
+    --variant "convnext_tiny"
 python train_stage_1.py
 python train_stage_2_3.py
